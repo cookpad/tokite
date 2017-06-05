fdea9aa0ca82eea051b5755179326ea99f53494b
hogelog <konbu.komuro@gmail.com>
2017-06-06 00:13:54 +0900

    Customizable emoji

--- config/initializers/slack_notifier.rb
+++ config/initializers/slack_notifier.rb
@@ -1,4 +1,4 @@
 Rails.application.config.slack_notifier = Slack::Notifier.new ENV["SLACK_WEBHOOK_URL"] do
   defaults username: ENV.fetch("SLACK_NAME", "tokite"),
-           icon_emoji: ":sunglasses:"
+           icon_emoji: ENV.fetch("SLACK_ICON_EMOJI", ":sunglasses:")
 end
