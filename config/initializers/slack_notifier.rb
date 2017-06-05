528b65eddc403bf7cb5d4d466e8a7ea7c74beda4
hogelog <konbu.komuro@gmail.com>
2017-06-06 00:06:57 +0900

    Remove unused default channel config

--- config/initializers/slack_notifier.rb
+++ config/initializers/slack_notifier.rb
@@ -1,5 +1,4 @@
 Rails.application.config.slack_notifier = Slack::Notifier.new ENV["SLACK_WEBHOOK_URL"] do
-  defaults channel: ENV.fetch("SLACK_DEFAULT_CHANNEL", "#general"),
-           username: ENV.fetch("SLACK_NAME", "tokite"),
+  defaults username: ENV.fetch("SLACK_NAME", "tokite"),
            icon_emoji: ":sunglasses:"
 end
