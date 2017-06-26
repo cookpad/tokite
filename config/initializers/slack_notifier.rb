4eea23d65b1c6aa0a0e08441d5dac9a3d530dcc5
hogelog <konbu.komuro@gmail.com>
2017-06-26 22:08:46 +0900

    Remove default icon_emoji

--- config/initializers/slack_notifier.rb
+++ config/initializers/slack_notifier.rb
@@ -1,6 +1,9 @@
 if ENV["SLACK_WEBHOOK_URL"]
   Tokite::Engine.config.slack_notifier = Slack::Notifier.new ENV["SLACK_WEBHOOK_URL"] do
-    defaults username: ENV.fetch("SLACK_NAME", "tokite"),
-             icon_emoji: ENV.fetch("SLACK_ICON_EMOJI", ":sunglasses:")
+    if ENV["SLACK_ICON_EMOJI"]
+      defaults username: ENV.fetch("SLACK_NAME", "tokite"), icon_emoji: ENV["SLACK_ICON_EMOJI"]
+    else
+      defaults username: ENV.fetch("SLACK_NAME", "tokite")
+    end
   end
 end
