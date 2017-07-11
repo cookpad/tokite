0e4edf29ec781fd199a07958ffa1f4dcc092caec
hogelog <konbu.komuro@gmail.com>
2017-07-12 00:47:03 +0900

    Fix omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 require "omniauth-google-oauth2"
 
-Rails.application.config.middleware.use OmniAuth::Builder do
+Tokite::Engine.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
     hd: ENV["GOOGLE_HOSTED_DOMAIN"].present? ? ENV["GOOGLE_HOSTED_DOMAIN"] : nil,
   }
