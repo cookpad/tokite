c638d36139b24ade5b987c4a0ffebe11564485e0
hogelog <konbu.komuro@gmail.com>
2017-07-16 09:23:15 +0900

    Validate slack channel

--- app/models/tokite/rule.rb
+++ app/models/tokite/rule.rb
@@ -6,6 +6,11 @@ module Tokite
   
     validate :validate_query
     validate :validate_user_id
+    validate :validate_channel
+
+    before_validation :normalize_channel
+
+    INVALID_CHANNEL_CHARS = [" ", ","]
 
     # TODO: Performance
     def self.matched_rules(event)
@@ -37,7 +42,9 @@ module Tokite
     def user_link
       "<#{Tokite::Engine.routes.url_helpers.user_url(user)}|#{user.name}>"
     end
-  
+
+    private
+
     def validate_query
       SearchQuery.parse(query)
     rescue SearchQuery::ParseError => e
@@ -47,6 +54,20 @@ module Tokite
     def validate_user_id
       unless User.find_by(id: user_id)
         errors.add(:user_id, "Unknown user_id: #{user_id}")
+      end
+    end
+
+    def validate_channel
+      INVALID_CHANNEL_CHARS.each do |invalid_char|
+        errors.add(:channel, %(Invalid character: "#{invalid_char}")) if channel.index(invalid_char)
+      end
+    end
+
+    def normalize_channel
+      if channel.start_with?("#")
+        self.channel = channel.strip
+      else
+        self.channel = "##{channel.strip}"
       end
     end
   end
