64afb19570f56fe7f6388c448f7e59cdfa14139f
hogelog <konbu.komuro@gmail.com>
2017-08-19 16:25:46 +0900

    Set dummy slack webhook url for test

--- config/initializers/slack_notifier.rb
+++ config/initializers/slack_notifier.rb
@@ -1,5 +1,11 @@
 if ENV["SLACK_WEBHOOK_URL"]
-  Tokite::Engine.config.slack_notifier = Slack::Notifier.new ENV["SLACK_WEBHOOK_URL"] do
+  webhook_url = ENV["SLACK_WEBHOOK_URL"]
+elsif Rails.env.test?
+  webhook_url = "https://example.com/notify"
+end
+
+if webhook_url
+  Tokite::Engine.config.slack_notifier = Slack::Notifier.new webhook_url do
     if ENV["SLACK_ICON_EMOJI"]
       defaults username: ENV.fetch("SLACK_NAME", "tokite"), icon_emoji: ENV["SLACK_ICON_EMOJI"]
     else
