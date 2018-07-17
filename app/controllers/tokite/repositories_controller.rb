13c923b2cce8c1087c82c068dd5897e25436b985
Daiki Chijiwa <daiki-chijiwa@cookpad.com>
2018-07-17 16:10:53 +0900

    can hook organizations

--- app/controllers/tokite/repositories_controller.rb
+++ app/controllers/tokite/repositories_controller.rb
@@ -3,37 +3,54 @@ module Tokite
     before_action :require_github_token, only: [:new, :create, :destroy]
 
     def index
-      @repositories = Repository.all
+      @orgs = []
+      @repositories = []
+      Repository.all.each do |item|
+        if item.is_org
+          @orgs << item
+        else
+          @repositories << item
+        end
+      end
     end
 
     def new
+      github_orgs = octokit_client.organization_memberships.
+        select{|membership| membership.role == "admin"}.
+        map{|membership| membership.organization }
       github_repos = octokit_client.repositories.
         select{|r| r.permissions.admin }.
         delete_if(&:fork).
         delete_if(&:archived)
+      @orgs = github_orgs.map do |org|
+        Repository.new(name: org.login, url: URI.join(ENV["GITHUB_HOST"], org.login).to_s, is_org: true)
+      end
       @repositories = github_repos.map do |repo|
         Repository.new(name: repo.full_name, url: repo.html_url, private: repo.private)
       end
       Repository.all.pluck(:name).each do |existing_name|
-        @repositories.delete_if {|repo| repo.name == existing_name }
+        @orgs.delete_if {|org| org.name == existing_name }
+        @repositories.
+          delete_if {|repo| repo.name == existing_name }.
+          delete_if {|repo| Repository.owner(repo.name) == existing_name }
       end
     end
 
     def create
-      if params[:names].nil?
-        flash[:error] = "Error: No repository was selected"
+      org_names = params[:org_names] || []
+      repo_names = params[:repo_names] || []
+      if repo_names.length + org_names.length == 0
+        flash[:error] = "Error: No organization/repository was selected"
       else
-        github_repos = params[:names].map do |name|
-          octokit_client.repository(name)
-        end
+        github_orgs = org_names.map{ |name| octokit_client.organization(name) }
+        github_repos = repo_names.map{ |name| octokit_client.repository(name) }
         errors = github_repos.select(&:archived).map(&:full_name)
         if errors != []
           flash[:error] = %(Error: The following repositories have been archived: #{errors.join(", ")})
         else
-          github_repos.each do |repo|
-            Repository.hook!(octokit_client, repo)
-          end
-          flash[:info] = "Import repositories."
+          github_orgs.each{ |repo| Repository.hook_org!(octokit_client, repo) }
+          github_repos.each{ |repo| Repository.hook_repo!(octokit_client, repo) }
+          flash[:info] = "Import organizations/repositories."
         end
       end
       redirect_to repositories_path
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 require "omniauth-github"
 
 Tokite::Engine.config.middleware.use OmniAuth::Builder do
-  options = { scope: "repo,write:repo_hook" }
+  options = { scope: "repo,write:repo_hook,read:orgs,admin:org_hook" }
   host = ENV["GITHUB_HOST"]
   if host.present?
     options.merge!(
