a5e20d4a464b5c2cdc8977b630a30edce7a64c34
hogelog <sunao-komuro@cookpad.com>
2019-12-27 12:33:00 +0900

    Fix factory syntax

--- spec/factories/tokite/rule_factory.rb
+++ spec/factories/tokite/rule_factory.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :rule, class: Tokite::Rule do
     user
     sequence(:name) {|n| "name_#{n}" }
-    query "query"
-    channel "#general"
+    query { "query" }
+    channel { "#general" }
   end
 end
