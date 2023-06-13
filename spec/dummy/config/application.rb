718e376c8355ad6c3b7cbb3efb7f6c9bde405bd0
nekketsuuu <nekketsuuu@users.noreply.github.com>
2023-06-13 20:13:34 +0900

    load_defaults 6.1

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -10,7 +10,7 @@ require "tokite"
 module Dummy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
     #
